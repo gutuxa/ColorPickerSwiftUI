8879accd4b883b4833661155f0637be1c97ca1ba
Oksana Tugusheva <gutuxa@gmail.com>
2021-06-06 17:25:43 +0300

    Initial Commit

---
+++ ColorPickerSwiftUI/ColorPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerSwiftUIApp.swift
+//  ColorPickerSwiftUI
+//
+//  Created by Oksana Tugusheva on 06.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
